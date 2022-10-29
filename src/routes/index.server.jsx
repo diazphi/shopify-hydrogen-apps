042c8515248066588e5bbfdcd5e7390422a77e57
Philip Diaz <diazphi@gmail.com>
2022-10-29 17:53:57 +0800

    updates components

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,3 +1,9 @@
+import FeaturedCollections from "../components/FeaturedCollections.server";
+import { Layout } from "../components/Layout.server";
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <Layout>
+       <FeaturedCollections />
+      </Layout>
+  );
 }
